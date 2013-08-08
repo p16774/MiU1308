14de1e5896c7c1552163f0a14358a7e93abc663c
Nathan Wharry <nobrakes@fullsail.edu>
2013-08-07 22:15:30 -0500

    20 dummy data
    Added the full 20 dummy data for MiU app.

--- wharry_nathan_project1/js/json.js
+++ wharry_nathan_project1/js/json.js
@@ -53,6 +53,244 @@ var json = {
 		"char_desc" : ["Description", "I am a ranger."],
 		"version" : ["Version", "3.5"]
 		
+	},
+
+	"character4" : {
+		
+		"char_name" : ["Name", "Simon"],
+		"char_race" : ["Race", "Human"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Wizard"],
+		"char_age" : ["Age", "18"],
+		"char_weigh" : ["Weight", "150"],
+		"char_birth" : ["BirthDay", "1985-02-09"],
+		"char_desc" : ["Description", "I am a wizard."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character5" : {
+		
+		"char_name" : ["Name", "Hank"],
+		"char_race" : ["Race", "Dwarf"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Ranger"],
+		"char_age" : ["Age", "23"],
+		"char_weigh" : ["Weight", "110"],
+		"char_birth" : ["BirthDay", "1990-07-24"],
+		"char_desc" : ["Description", "I am a ranger."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character6" : {
+		
+		"char_name" : ["Name", "Giselle"],
+		"char_race" : ["Race", "Elf"],
+		"char_gen" : ["Gender", "Female"],
+		"char_class" : ["Class", "Sorceror"],
+		"char_age" : ["Age", "15"],
+		"char_weigh" : ["Weight", "90"],
+		"char_birth" : ["BirthDay", "2000-04-13"],
+		"char_desc" : ["Description", "I am a sorceror."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character7" : {
+		
+		"char_name" : ["Name", "Jocelyn"],
+		"char_race" : ["Race", "Half-Orc"],
+		"char_gen" : ["Gender", "Female"],
+		"char_class" : ["Class", "Barbarian"],
+		"char_age" : ["Age", "27"],
+		"char_weigh" : ["Weight", "178"],
+		"char_birth" : ["BirthDay", "1996-04-16"],
+		"char_desc" : ["Description", "I am a barbarian."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character8" : {
+		
+		"char_name" : ["Name", "Ozy"],
+		"char_race" : ["Race", "Elf"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Wizard"],
+		"char_age" : ["Age", "24"],
+		"char_weigh" : ["Weight", "130"],
+		"char_birth" : ["BirthDay", "1990-04-17"],
+		"char_desc" : ["Description", "I am a wizard."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character9" : {
+		
+		"char_name" : ["Name", "Kaedus"],
+		"char_race" : ["Race", "Human"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Cleric"],
+		"char_age" : ["Age", "28"],
+		"char_weigh" : ["Weight", "157"],
+		"char_birth" : ["BirthDay", "1994-05-03"],
+		"char_desc" : ["Description", "I am a cleric."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character10" : {
+		
+		"char_name" : ["Name", "Ori"],
+		"char_race" : ["Race", "Half-Orc"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Ranger"],
+		"char_age" : ["Age", "33"],
+		"char_weigh" : ["Weight", "167"],
+		"char_birth" : ["BirthDay", "1985-06-01"],
+		"char_desc" : ["Description", "I am a ranger."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character11" : {
+		
+		"char_name" : ["Name", "Grokk"],
+		"char_race" : ["Race", "Human"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Fighter"],
+		"char_age" : ["Age", "28"],
+		"char_weigh" : ["Weight", "178"],
+		"char_birth" : ["BirthDay", "1990-07-04"],
+		"char_desc" : ["Description", "I am a fighter."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character12" : {
+		
+		"char_name" : ["Name", "Kratos"],
+		"char_race" : ["Race", "Dwarf"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Fighter"],
+		"char_age" : ["Age", "33"],
+		"char_weigh" : ["Weight", "100"],
+		"char_birth" : ["BirthDay", "1985-12-24"],
+		"char_desc" : ["Description", "I am a fighter."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character13" : {
+		
+		"char_name" : ["Name", "Randall"],
+		"char_race" : ["Race", "Halfling"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Bard"],
+		"char_age" : ["Age", "18"],
+		"char_weigh" : ["Weight", "45"],
+		"char_birth" : ["BirthDay", "1996-07-15"],
+		"char_desc" : ["Description", "I am a bard."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character14" : {
+		
+		"char_name" : ["Name", "Gwen"],
+		"char_race" : ["Race", "Halfling"],
+		"char_gen" : ["Gender", "Female"],
+		"char_class" : ["Class", "Bard"],
+		"char_age" : ["Age", "22"],
+		"char_weigh" : ["Weight", "35"],
+		"char_birth" : ["BirthDay", "1993-08-10"],
+		"char_desc" : ["Description", "I am a bard."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character15" : {
+		
+		"char_name" : ["Name", "Yancy"],
+		"char_race" : ["Race", "Dwarf"],
+		"char_gen" : ["Gender", "Female"],
+		"char_class" : ["Class", "Barbarian"],
+		"char_age" : ["Age", "27"],
+		"char_weigh" : ["Weight", "85"],
+		"char_birth" : ["BirthDay", "1990-09-20"],
+		"char_desc" : ["Description", "I am a barbarian."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character16" : {
+		
+		"char_name" : ["Name", "Bradley"],
+		"char_race" : ["Race", "Human"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Ranger"],
+		"char_age" : ["Age", "33"],
+		"char_weigh" : ["Weight", "156"],
+		"char_birth" : ["BirthDay", "1985-12-24"],
+		"char_desc" : ["Description", "I am a ranger."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character17" : {
+		
+		"char_name" : ["Name", "Mistle"],
+		"char_race" : ["Race", "Elf"],
+		"char_gen" : ["Gender", "Female"],
+		"char_class" : ["Class", "Ranger"],
+		"char_age" : ["Age", "26"],
+		"char_weigh" : ["Weight", "110"],
+		"char_birth" : ["BirthDay", "1991-09-18"],
+		"char_desc" : ["Description", "I am a ranger."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character18" : {
+		
+		"char_name" : ["Name", "Markwell"],
+		"char_race" : ["Race", "Dwarf"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Cleric"],
+		"char_age" : ["Age", "54"],
+		"char_weigh" : ["Weight", "135"],
+		"char_birth" : ["BirthDay", "1965-11-10"],
+		"char_desc" : ["Description", "I am a cleric."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character19" : {
+		
+		"char_name" : ["Name", "Ulysseus"],
+		"char_race" : ["Race", "Human"],
+		"char_gen" : ["Gender", "Male"],
+		"char_class" : ["Class", "Barbarian"],
+		"char_age" : ["Age", "45"],
+		"char_weigh" : ["Weight", "186"],
+		"char_birth" : ["BirthDay", "1970-10-20"],
+		"char_desc" : ["Description", "I am a barbarian."],
+		"version" : ["Version", "3.5"]
+		
+	},
+
+	"character20" : {
+		
+		"char_name" : ["Name", "Lister"],
+		"char_race" : ["Race", "Halfling"],
+		"char_gen" : ["Gender", "Female"],
+		"char_class" : ["Class", "Wizard"],
+		"char_age" : ["Age", "40"],
+		"char_weigh" : ["Weight", "175"],
+		"char_birth" : ["BirthDay", "1980-12-10"],
+		"char_desc" : ["Description", "I am a wizard."],
+		"version" : ["Version", "3.5"]
+		
 	}
 	
 };

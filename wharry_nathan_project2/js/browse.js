561d03a2367b1c901331e34cc2aa81f406e013cb
Nathan Wharry <nobrakes@fullsail.edu>
2013-08-11 22:13:42 -0500

    Project 2 and Navigation
    Created copy for project 2 and redesigning navigation structure. Trying
    to implement some tabbed type browsing and getting the links to
    function properly when clicked from other pages by adding HASH values
    to nav links.

---
+++ wharry_nathan_project2/js/browse.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+/*
+
+*********************
+Author: Nathan Wharry
+Title: Project 2 - D&D Character Sheet - Browse Javascript
+Term: MiU 1308
+*********************
+
+*/
+
+// Wait for DOM to fully load
+window.addEventListener("DOMContentLoaded", function() {
+	
+	// Define browse options
+	searchItem = window.location.hash;
+	
+	alert(searchItem);
+	
+	
+	// Browse by Name
+	function browseName() {
+		
+		alert("This is browsing by name");
+		
+	};
+	
+	
+	
+	
+	
+	
+	
+
+
+}); // end Wait For Page to Load function
